af6b8bf478b8707b34ae24dc82608f784c309420
onglu1 <34936104+onglu1@users.noreply.github.com>
2022-03-19 19:00:19 +0800

    20220313

---
+++ codelist/Acwing/第四章 计算几何/1401. 围住奶牛.cpp
@@ -0,0 +1,108 @@
+//
+// Created by onglu on 2022/3/18.
+//
+
+#include <bits/stdc++.h>
+
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define nxt(i) ((i + 1) % s.size())
+
+#define endl '\n'
+#define lson (rt << 1)
+#define rson (rt << 1 | 1)
+#define Mid ((l + r) / 2)
+//#define int long long
+using namespace std;
+const int N = 2e6 + 1009;
+const double Pi = acos(-1.0);
+const double eps = 1e-11;
+int sgn(double x) {
+    if(fabs(x) < eps) return 0;
+    else return x < 0 ? -1 : 1;
+}
+struct line;
+struct Point;
+struct Point {
+    double x, y;
+    Point() : x(0), y(0) {}
+    Point(double x, double y) : x(x), y(y) {}
+    Point(const line &l);
+};
+struct line{
+    Point s, t;
+    line() {}
+    line(const Point &s, const Point &t) : s(s), t(t) {}
+};
+typedef Point Vector;
+Point operator+(const Point &a, const Point &b) { return Point(a.x + b.x, a.y + b.y); }
+Point operator-(const Point &a, const Point &b) { return Point(a.x - b.x, a.y - b.y); }
+Point operator*(const Point &a, const double &c) { return Point(c * a.x, c * a.y); }
+Point operator/(const Point &a, const double &c) { return Point(a.x / c, a.y / c); }
+inline bool operator < (const Point &a, const Point &b) {
+    return sgn(a.x - b.x) < 0 || (sgn(a.x - b.x) == 0 && sgn(a.y - b.y) < 0);
+}
+Point :: Point(const line &l) { *this = l.t - l.s; }
+bool operator == (const Point& a, const Point& b) { return !sgn(a.x - b.x) && !sgn(a.y - b.y); }
+double dot(const Vector& a, const Vector& b) { return a.x * b.x + a.y * b.y; }
+double det(const Vector& a, const Vector& b) { return a.x * b.y - a.y * b.x; }
+double cross(const Point& s, const Point& t, const Point& o = Point()) { return det(s - o, t - o); }
+double abs(const Point &a){ return sqrt(a.x * a.x + a.y * a.y); }
+Point rot(const Point &a, double theta){ return Point(a.x * cos(theta) - a.y * sin(theta), a.x * sin(theta) + a.y * cos(theta)); }
+Point rotCCW90(const Point &a) { return Point(-a.y, a.x); }
+Point rotCW90(const Point &a) { return Point(a.y, -a.x); }
+
+int n;
+typedef vector<Point> Polygon;
+Polygon Convex_hull(Polygon &s) {
+    sort(s.begin(), s.end());
+    int un = 0;
+    for(int i = 0; i < s.size(); i++) {
+        if(i == 0 || sgn(dot(s[i] - s[i - 1], s[i] - s[i - 1])) != 0) {
+            s[un++] = s[i];
+        }
+    }
+    s.resize(un);
+    Polygon ret(s.size() * 2);
+    int sz = 0;
+    for(int i = 0; i < s.size(); i++) {
+        while(sz > 1 && sgn(cross(ret[sz - 1], s[i], ret[sz - 2])) < 0) sz--;
+        ret[sz++] = s[i];
+    }
+    int k = sz;
+    for(int i = s.size() - 2; i >= 0; i--) {
+        while(sz > 1 && sgn(cross(ret[sz - 1], s[i], ret[sz - 2])) < 0) sz--;
+        ret[sz++] = s[i];
+    }
+    ret.resize(sz - (s.size() > 1));
+    return ret;
+}
+void work() {
+    scanf("%d", &n);
+    Polygon p;
+    for(int i = 1; i <= n; i++) {
+        double x, y;
+        scanf("%lf %lf", &x, &y);
+        p.push_back({x, y});
+    }
+    Polygon s = Convex_hull(p);
+    double ans = 0;
+    for(int i = 0; i < s.size(); i++) {
+        ans += abs(s[i] - s[nxt(i)]);
+        cerr << s[i].x << " " << s[i].y << endl;
+    }
+    printf("%.2f\n", ans);
+
+
+}
+
+signed main() {
+#ifdef LOCAL
+    freopen("C:\\Users\\onglu\\CLionProjects\\acm\\data.in", "r", stdin);
+    freopen("C:\\Users\\onglu\\CLionProjects\\acm\\data.out", "w", stdout);
+#endif
+//    ios::sync_with_stdio(false);
+//    cin.tie(0);
+    work();
+    return 0;
+}
--- codelist/other/2022-天梯赛训练赛四/15.cpp
+++ codelist/other/2022-天梯赛训练赛四/15.cpp
@@ -17,11 +17,8 @@ const int N = 2e6 + 1009;
 //const int N = 5009;
 //const int N = 309
 int n, m, a[N];
-
-void work() {
-
-}
-
+int x[N], y[N];
+int g[20][20], f[20][20];
 signed main() {
 #ifdef LOCAL
     freopen("C:\\Users\\onglu\\CLionProjects\\acm\\data.in", "r", stdin);
@@ -29,8 +26,29 @@ signed main() {
 #endif
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int Case = 0;
-    cin >> Case;
-    while (Case--) work();
+    n = 4;
+    x[1] = 20; y[1] = 25;
+    x[2] = 25; y[2] = 10;
+    x[3] = 10; y[3] = 5;
+    x[4] = 5; y[4] = 15;
+    memset(f, 0x3f, sizeof(f));
+    for(int i = 1; i + 1 <= n; i++) {
+        f[i][i] = 0;
+        f[i][i + 1] = x[i] * y[i] * y[i + 1];
+    }
+    f[n][n] = 0;
+    for(int len = 3; len <= n; len++) {
+        for(int i = 1; i + len - 1 <= n; i++) {
+            int j = i + len - 1;
+            for(int k = i; k < j; k++) {
+                if(f[i][k] + f[k + 1][j] + x[i] * y[k] * y[j] < f[i][j]) {
+                    f[i][j] = f[i][k] + f[k + 1][j] + x[i] * y[k] * y[j];
+                    g[i][j] = k;
+                }
+            }
+        }
+    }
+    cout << f[1][n] << endl;
+    cout << g[1][n] << endl;
     return 0;
 }
